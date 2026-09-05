355e3cdb3cf60780b9d3c74707a225ff813e0b28
Simran Kaur <simkaur2637@gmail.com>
2026-09-05 18:29:50 +0530

    Create operators.cpp
    added program on different operators

---
+++ operators.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    int b = 3;
+
+    cout << "Addition: " << a + b << endl;
+    cout << "Subtraction: " << a - b << endl;
+    cout << "Multiplication: " << a * b << endl;
+    cout << "Division: " << a / b << endl;
+    cout << "Remainder: " << a % b << endl;
+
+    return 0;
+}
