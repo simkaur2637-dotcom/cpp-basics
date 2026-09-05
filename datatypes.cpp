0940e0b75f0428d47be224360810eb07b78e1904
Simran Kaur <simkaur2637@gmail.com>
2026-09-05 18:27:26 +0530

    Create datatypes.cpp
    code added on variables

---
+++ datatypes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 19;
+    float height = 5.5;
+    double price = 99.99;
+    char grade = 'A';
+    bool isStudent = true;
+
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "Price: " << price << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Student: " << isStudent << endl;
+
+    return 0;
+}
