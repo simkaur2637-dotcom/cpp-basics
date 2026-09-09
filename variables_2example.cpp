376e2ad0b43d5ec3a8cec0879afea863d1d4a4ce
Simran Kaur <simkaur2637@gmail.com>
2026-09-09 21:39:23 +0530

    Create variables_2example.cpp
    real life examples of variables and data type

---
+++ variables_2example.cpp
@@ -0,0 +1,22 @@
+// calculate area of rectangle 
+int length = 4;
+int width = 6;
+int area;
+
+area = length*width;
+
+cout<<"length is: << length << "\n";
+cout << "Width is: " << width << "\n";
+cout << "Area of the rectangle is: " << area << "\n";
+
+//Use different data types to calculate and output the total cost of a number of items:
+// Create variables of different data types
+int items = 50;
+double cost_per_item = 9.99;
+double total_cost = items * cost_per_item;
+char currency = '$';
+
+// Print variables
+cout << "Number of items: " << items << "\n";
+cout << "Cost per item: " << cost_per_item << "" << currency << "\n";
+cout << "Total cost = " << total_cost << "" << currency << "\n";
